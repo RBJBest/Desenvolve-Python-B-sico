490487a274ff4a0d346c70901fc66b3f0704b48b
Ricardo Bruno Januário <163652053+RBJBest@users.noreply.github.com>
2024-06-21 01:14:28 -0300

    Add files via upload
    Exercícios Modulo 1_ Projeto Desenvolve

---
+++ Exercícios Modulo 1/1.2 Algoritimo/aula1_ questao1.py
@@ -0,0 +1,55 @@
+print ("TABUADA DO 09") 
+n=0
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
+
+n+=1
+digito1= n-1
+digito2= 10-n
+print (f"9 x {n} = {digito1*10+digito2}")
